852cb34f7fafdf196b7a6f42d0720f42cffaa276
Nomsluier <93717074+PS228607@users.noreply.github.com>
2023-07-13 23:16:05 +0200

    Dictionary's
    List
    Tree
    class

---
+++ MMGuide_binaryTree/MMGuid_binaryTree/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace BinaryTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new BinaryTree
+            {
+                //Maakt mijn boomstructuur
+                //'Root' word als beginpunt gebruikt
+                Root = new BinaryTreeNode
+                {
+                    Value = 8,
+                    Left = new BinaryTreeNode
+                    {
+                        Value = 3,
+                        Left = new BinaryTreeNode
+                        {
+                            Value = 1
+                        }
+                    },
+
+
+                    Right = new BinaryTreeNode
+                    {
+                        Value = 9,
+                        Left = new BinaryTreeNode
+                        {
+                            Value = 6,
+                            Left = new BinaryTreeNode
+                            {
+                                Value = 7,
+                            },
+                            Right = new BinaryTreeNode
+                            {
+                                Value = 4
+                            }
+                        },
+
+                        Right = new BinaryTreeNode
+                        {
+                            Value = 10,
+                            Left = new BinaryTreeNode
+                            { 
+                                Value = 2,
+                            }
+                        }
+                    }
+                }
+            };
+
+            Console.WriteLine("Enkelvoudige kindknooppunten:");
+            tree.PrintSingleChildLeaves();
+        }
+    }
+
+
+    public class BinaryTreeNode
+    {
+        public int Value { get; set; }
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+    }
+    public class BinaryTree
+    {
+        public BinaryTreeNode Root { get; set; }
+
+        public void PrintSingleChildLeaves()
+        {
+            PrintSingleChildLeaves(Root);
+        }
+
+        private void PrintSingleChildLeaves(BinaryTreeNode node)
+        {
+            if (node == null) //Controleert of de boom een huidig knooppunt heeft
+                return;
+
+
+            if ((node.Left == null && node.Right != null && node.Right.Left == null && node.Right.Right == null) ||
+                (node.Right == null && node.Left != null && node.Left.Left == null && node.Left.Right == null))
+            {
+                Console.WriteLine(node.Value); //Geeft huidige knooppunt aan
+            }
+
+            PrintSingleChildLeaves(node.Left);
+            PrintSingleChildLeaves(node.Right);
+
+            /*Gaat net zo lang door ded methode voor linker en rechterkind,
+            Tot dat alle enkelvoudige knooppunten bekend zijn*/
+        }
+    }
+}
---
+++ MMGuide_class/MMGuide/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Optimaliseer de code op zo’n manier dat je AnimalList en DoubleList kan vervangen met een 
+    klasse GenericList met een enkele DoSomething methode.
+*/
+public class MMGuide_1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Console test");
+
+        GenericList<object> listOfAll = new GenericList<object>();
+        listOfAll.DoSomething(2.5);
+        listOfAll.DoSomething(new Animal());
+    }
+}
+
+public class Animal
+{
+    public string Name { get; set; }
+    public List<string> Habitats { get; set; }
+}
+
+public class GenericList<T>
+{
+    public void DoSomething(T item)
+    {
+        Console.WriteLine($"Doing something with {item}");
+    }
+}
+
+
+/*public class AnimalList
+{
+    public void DoSomething(Animal animal)
+    {
+        Console.WriteLine("Foo Animal");
+    }
+}
+public class DoubleList
+{
+    public void DoSomething(double number)
+    {
+        Console.WriteLine("Foo Number");
+    }
+}*/
